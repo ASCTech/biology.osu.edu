f99d6da1eb2f2178fd78c052514358b2e2d4f720
Michael Gee <gee.24@osu.edu>
2007-05-17 16:32:17 +0000

    fixed html base staff directory emails are now links

--- contact/staff.php
+++ contact/staff.php
@@ -5,46 +5,46 @@ top('Staff Directory');
 no_menu();
 
 $people = array(
- array('Caroline Breitenberger',
-       'Director',
-       'breitenberger.1@osu.edu',
-       '(614) <span style="white-space:nowrap">292-6945</span>'),
- array('Judy Ridgway',
-       'Assistant&nbsp;Director (Assessment&nbsp;&amp;&nbsp;Pedagogy)',
-       'ridgway.14@osu.edu',
-	   '(614) <span style="white-space:nowrap">292-0837</span>'),
- array('Matt Misicka',
-       'Assistant&nbsp;Director (Introductory&nbsp;Biology)<br/>&nbsp; &amp; Course Coordinator (102)',
-       'misicka.1@osu.edu',
-	   '(614) <span style="white-space:nowrap">688-3466</span>'),
- array('Kathy Royer',
-       'Office Manager',
-       'royer.1@osu.edu',
-       '(614) <span style="white-space:nowrap">292-0535</span>'),
- array('Rosemarie Thornton',
-       'Office Associate',
-       'thornton.2@osu.edu',
-       '(614) <span style="white-space:nowrap">292-9861</span>'),
- array('Adam Andrews',
-       'Course Coordinator (101)',
-       'andrews.171@osu.edu',
-       '(614) <span style="white-space:nowrap">247-6345</span>'),
- array('Tiffany Langenderfer',
-       'Assistant Course Coordinator (101)',
-       'langenderfer.19@osu.edu',
-       '(614) <span style="white-space:nowrap">247-8087</span>'),
- array('Betsy Wrobel-Boerner',
-       'Course Coordinator (113,&nbsp;H115)',
-       '<span style="white-space:nowrap">wrobel-boerner.1@osu.edu</span>',
-	   '(614) <span style="white-space:nowrap">688-5495</span>'),
- array('Amy Kovach',
-       'Course Coordinator (114,&nbsp;H116)',
-       'kovach.71@osu.edu',
-       '(614) <span style="white-space:nowrap">292-3929</span>'),
- array('Michael Gee',
-       'Learning Technology',
-       'gee.24@osu.edu',
-       '(614) <span style="white-space:nowrap">247-6576</span>'),
+ array('name' => 'Caroline Breitenberger',
+       'title' => 'Director',
+       'email' => 'breitenberger.1@osu.edu',
+       'phone' => '292-6945'),
+ array('name' => 'Judy Ridgway',
+       'title' => 'Assistant&nbsp;Director (Assessment&nbsp;&amp;&nbsp;Pedagogy)',
+       'email' => 'ridgway.14@osu.edu',
+	   'phone' => '292-0837'),
+ array('name' => 'Matt Misicka',
+       'title' => 'Assistant&nbsp;Director (Introductory&nbsp;Biology)<br/>&nbsp; &amp; Course Coordinator (102)',
+       'email' => 'misicka.1@osu.edu',
+	   'phone' => '688-3466'),
+ array('name' => 'Kathy Royer',
+       'title' => 'Office Manager',
+       'email' => 'royer.1@osu.edu',
+       'phone' => '292-0535'),
+ array('name' => 'Rosemarie Thornton',
+       'title' => 'Office Associate',
+       'email' => 'thornton.2@osu.edu',
+       'phone' => '292-9861'),
+ array('name' => 'Adam Andrews',
+       'title' => 'Course Coordinator (101)',
+       'email' => 'andrews.171@osu.edu',
+       'phone' => '247-6345'),
+ array('name' => 'Tiffany Langenderfer',
+       'title' => 'Assistant Course Coordinator (101)',
+       'email' => 'langenderfer.19@osu.edu',
+       'phone' => '247-8087'),
+ array('name' => 'Betsy Wrobel-Boerner',
+       'title' => 'Course Coordinator (113,&nbsp;H115)',
+       'email' => 'wrobel-boerner.1@osu.edu',
+	   'phone' => '688-5495'),
+ array('name' => 'Amy Kovach',
+       'title' => 'Course Coordinator (114,&nbsp;H116)',
+       'email' => 'kovach.71@osu.edu',
+       'phone' => '292-3929'),
+ array('name' => 'Michael Gee',
+       'title' => 'Learning Technology',
+       'email' => 'gee.24@osu.edu',
+       'phone' => '247-6576'),
 );
 
 $n = 0;
@@ -58,9 +58,10 @@ $n = 0;
  </tr>
 <?   while ($person = array_shift($people)): ?>
  <tr class="r<?= $n = 1 - $n ?>">
-<?     while ($datum = array_shift($person)): ?>
-  <td><?= $datum ?></td>
-<?     endwhile; ?>
+  <td><?= $person['name'] ?></td>
+  <td><?= $person['title'] ?></td>
+  <td><span style="white-space:nowrap"><a href="mailto:<?= $person['name'] ?> <<?= $person['email'] ?>>" class="email"><?= $person['email'] ?></a></span></td>
+  <td>(614) <span style="white-space:nowrap"><?= $person['phone'] ?></span></td>
  </tr>
 <?   endwhile; ?>
 </table>
