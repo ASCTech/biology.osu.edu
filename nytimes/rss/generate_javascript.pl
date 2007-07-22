ea2c1d4862fb7056d3fe815eea46922cc648ba26
biology on excelsior <biology@osu.edu>
2007-07-22 13:05:35 +0000

    fixed svn settings for nytimes/rss/*

--- nytimes/rss/generate_javascript.pl
+++ nytimes/rss/generate_javascript.pl
@@ -13,19 +13,71 @@ use strict;
 use warnings;
 
 use XML::RSS::JavaScript;
-use LWP::Simple;
+use LWP::Simple qw( mirror );
 
 use Fatal qw( open );
 
-my $url   = 'http://www.nytimes.com/services/xml/rss/nyt/Science.xml';
-my $cache = 'science.js';
+my $xml_url  = 'http://www.nytimes.com/services/xml/rss/nyt/Science.xml';
+my $xml_file = 'science.xml';
+my $js_file  = 'science.js';
+my $number_of_headlines = 10;
+my $include_descriptions = 1;
 
-my $xml = get($url);
-my $rss = XML::RSS::JavaScript->new();
-$rss->parse($xml);
-my $js = $rss->as_javascript(10, 1);
-if (length $js) {
-  open(my $fh, '>', $cache);
-  print {$fh} $js;
+if (download($xml_url, $xml_file)) {
+  my $xml = read_file($xml_file);
+  my $js = xml2js($xml);
+  write_file($js_file, $js) if $js;
+
+  append(mtime($xml_file), 'mtimes.log');
+}
+exit;
+
+sub xml2js {
+  my $xml = shift;
+  my $rss = XML::RSS::JavaScript->new;
+  $rss->parse($xml);
+  $rss->as_javascript($number_of_headlines, $include_descriptions);
+}
+
+sub download {
+  my ($url, $file) = @_;
+  my $age = age($file);
+  mirror($url, $file);
+  $age ne age($file);
+}
+
+sub age {
+  my $file = shift;
+  time - mtime($file);
+}
+
+sub mtime {
+  my $file = shift;
+  -e $file ? (stat $file)[9] : 0;
+}
+
+sub read_file {
+  my $file = shift;
+  my $content;
+  open(my $fh, '<', $file);
+  {
+    local $/;
+    $content = <$fh>;
+  }
+  close($fh);
+  $content;
+}
+
+sub write_file {
+  my $file = shift;
+  open(my $fh, '>', $file);
+  print {$fh} @_;
+  close($fh);
+}
+
+sub append {
+  my ($line, $file) = @_;
+  open(my $fh, '>>', $file);
+  print {$fh} "$line\n";
   close($fh);
 }
