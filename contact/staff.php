2a1bd532eafc8e3709d4bbee10b583081f9797f3
Michael Gee <gee.24@osu.edu>
2007-08-01 12:35:57 +0000

    added Barbara, Peggy, David, and David to Staff Directory auto-sort Directory by last name

--- contact/staff.php
+++ contact/staff.php
@@ -5,47 +5,84 @@ top('Staff Directory');
 no_menu();
 
 $people = array(
- array('name' => 'Caroline Breitenberger',
+ array('first' => 'Caroline',
+       'last'  => 'Breitenberger',
        'title' => 'Director',
        'email' => 'breitenberger.1@osu.edu',
        'phone' => '292-6945'),
- array('name' => 'Judy Ridgway',
+ array('first' => 'Judy',
+       'last'  => 'Ridgway',
        'title' => 'Assistant&nbsp;Director (Assessment&nbsp;&amp;&nbsp;Pedagogy)',
        'email' => 'ridgway.14@osu.edu',
 	   'phone' => '292-0837'),
- array('name' => 'Matt Misicka',
+ array('first' => 'Matt',
+       'last'  => 'Misicka',
        'title' => 'Assistant&nbsp;Director (Introductory&nbsp;Biology)<br/>&nbsp; &amp; Course Coordinator (102)',
        'email' => 'misicka.1@osu.edu',
 	   'phone' => '688-3466'),
- array('name' => 'Kathy Royer',
+ array('first' => 'Kathy',
+       'last'  => 'Royer',
        'title' => 'Office Manager',
        'email' => 'royer.1@osu.edu',
        'phone' => '292-0535'),
- array('name' => 'Rosemarie Thornton',
-       'title' => 'Office Associate',
+ array('first' => 'Rosemarie',
+       'last'  => 'Thornton',
+       'title' => 'Office Associate (Introductory Biology)',
        'email' => 'thornton.2@osu.edu',
        'phone' => '292-9861'),
- array('name' => 'Adam Andrews',
+ array('first' => 'Adam',
+       'last'  => 'Andrews',
        'title' => 'Course Coordinator (101)',
        'email' => 'andrews.171@osu.edu',
        'phone' => '247-6345'),
- array('name' => 'Tiffany Langenderfer',
+ array('first' => 'Tiffany',
+       'last'  => 'Langenderfer',
        'title' => 'Assistant Course Coordinator (101)',
        'email' => 'langenderfer.19@osu.edu',
        'phone' => '247-8087'),
- array('name' => 'Betsy Wrobel-Boerner',
+ array('first' => 'Betsy',
+       'last'  => 'Wrobel-Boerner',
        'title' => 'Course Coordinator (113,&nbsp;H115)',
        'email' => 'wrobel-boerner.1@osu.edu',
 	   'phone' => '688-5495'),
- array('name' => 'Amy Kovach',
+ array('first' => 'Amy',
+       'last'  => 'Kovach',
        'title' => 'Course Coordinator (114,&nbsp;H116)',
        'email' => 'kovach.71@osu.edu',
        'phone' => '292-3929'),
- array('name' => 'Michael Gee',
-       'title' => 'Learning Technology',
+ array('first' => 'Michael',
+       'last'  => 'Gee',
+       'title' => 'Educational Systems Developer',
        'email' => 'gee.24@osu.edu',
        'phone' => '247-6576'),
+ array('first' => 'Barbara',
+       'last'  => 'Eakins',
+       'title' => 'Office Associate (Academic Advising)',
+	   'email' => 'eakins.16@osu.edu',
+	   'phone' => '292-1704'),
+ array('first' => 'David',
+       'last'  => 'Wells',
+       'title' => 'Academic Advisor',
+	   'email' => 'wells.252@osu.edu',
+	   'phone' => '292-1704'),
+ array('first' => 'Peggy',
+       'last'  => 'Strow',
+       'title' => 'Academic Advisor',
+	   'email' => 'strow.1@osu.edu',
+	   'phone' => '292-1704'),
+ array('first' => 'David',
+       'last'  => 'Stetson',
+       'title' => 'Associate Director',
+	   'email' => 'stetson.1@osu.edu',
+	   'phone' => '292-5307'),
 );
+
+function cmp($a, $b) {
+	$input = $sorted = array($a['last'], $b['last']);
+	sort($sorted);
+	return $input == $sorted ? -1 : 1;
+}
+usort($people, 'cmp');
 
 $n = 0;
 ?>
@@ -58,9 +95,9 @@ $n = 0;
  </tr>
 <?   while ($person = array_shift($people)): ?>
  <tr class="r<?= $n = 1 - $n ?>">
-  <td><?= $person['name'] ?></td>
+  <td><?= $person['first'] ?> <?= $person['last'] ?></td>
   <td><?= $person['title'] ?></td>
-  <td><span style="white-space:nowrap"><a href="mailto:<?= $person['name'] ?> <<?= $person['email'] ?>>" class="email"><?= $person['email'] ?></a></span></td>
+  <td><span style="white-space:nowrap"><a href="mailto:<?= $person['first'] ?> <?= $person['last'] ?> <<?= $person['email'] ?>>" class="email"><?= $person['email'] ?></a></span></td>
   <td>(614) <span style="white-space:nowrap"><?= $person['phone'] ?></span></td>
  </tr>
 <?   endwhile; ?>
