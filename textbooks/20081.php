7c4a22d5dfae7b7b7a4976d7324dcdc9c8fe628a
Michael Gee <gee.24@osu.edu>
2007-12-14 20:48:47 +0000

    WI2008 Textbooks

---
+++ textbooks/20081.php
@@ -0,0 +1,136 @@
+<?
+chdir('..');
+include_once 'lib/basics.inc';
+top('Textbooks','Winter 2008');
+menu('textbooks');
+?>
+<table class="textbooks" cellspacing="0" cellpadding="0">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Lanno, Wenzel, Taylor</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>101 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2339-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>102</td>
+  <td>Gough, Chordas</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>113</td>
+  <td>Cogan &amp; Weinstein</td>
+  <td>Biology, 7th edition</td>
+  <td>Campbell &amp; Reece</td>
+  <td class="isbn">978-0-8053-7146-X</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>113/115 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2355-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>114</td>
+  <td>Hetherington</td>
+  <td>Biology, 7th edition</td>
+  <td>Campbell &amp; Reece</td>
+  <td class="isbn">978-0-8053-7146-X</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>114 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2349-6</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td />
+  <td />
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>H115</td>
+  <td>Ding &amp; Simcox</td>
+  <td>Biology, 7th edition</td>
+  <td>Campbell &amp; Reece</td>
+  <td class="isbn">978-0-8053-7146-X</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>113/115 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2355-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>H116</td>
+  <td>Boerner</td>
+  <td>Biology</td>
+  <td>Brooker,&nbsp;Robert, et.al.</td>
+  <td class="isbn">978-0-07-295620-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>116 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2644-2</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Skin, A Natural History</td>
+  <td>Jablonski</td>
+  <td class="isbn">978-0-520-24281-4</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise Dissection, Sheep Brain, Pig Heart</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise Dissection, Pigs</td>
+  <td />
+  <td />
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
