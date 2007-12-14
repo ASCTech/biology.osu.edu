8c2d490f9fa2e7706212bbf23a2f6a7672af6c00
Michael Gee <gee.24@osu.edu>
2007-12-14 20:55:02 +0000

    mention major in front page text

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ no_menu();
    administered by the <a href="http://www.biosci.ohio-state.edu/">College of
    Biological Sciences</a> at The Ohio State University. The Center's 
    mission is to develop, teach, and provide administrative support for
-   Biology courses taught by the College of Biological Sciences.</p>
+   the Biology major and the Biology courses taught by the College of Biological
+   Sciences.</p>
 <p>The introductory level majors and non-majors courses taught by the CLSE are
    an important component of the GEC of the Colleges of the Arts and Sciences.
    Within the paradigm of an arts and sciences curriculum, it is the mission of
