8469543af0f1e0b608c1aabd304b0954b2c18954
Michael Gee <gee.24@osu.edu>
2007-12-14 20:20:01 +0000

    CLSE -> Biology Programs

--- index.php
+++ index.php
@@ -6,15 +6,9 @@ no_menu();
 
 <p>The Center for Life Sciences Education is an interdepartmental program
    administered by the <a href="http://www.biosci.ohio-state.edu/">College of
-   Biological Sciences</a> at The Ohio State University. The Center's basic
+   Biological Sciences</a> at The Ohio State University. The Center's 
    mission is to develop, teach, and provide administrative support for
-   introductory-level Biology courses taught by the College of Biological
-   Sciences. These courses include those that satisfy the General Education
-   Curriculum (GEC) requirement for students who are not majoring in the
-   biological sciences (Biology 101&ndash;102), for students who are planning on
-   majoring in one of the biological sciences (Biology 113&ndash;114), an Honors
-   sequence (Biology H115&ndash;H116), and other Biology courses that might be developed
-   and taught.</p>
+   Biology courses taught by the College of Biological Sciences.</p>
 <p>The introductory level majors and non-majors courses taught by the CLSE are
    an important component of the GEC of the Colleges of the Arts and Sciences.
    Within the paradigm of an arts and sciences curriculum, it is the mission of
--- nytimes/index.php
+++ nytimes/index.php
@@ -3,15 +3,14 @@ chdir('..');
 include_once 'lib/basics.inc';
 top('New York Times');
 menu('nyt');
+include 'nytimes/usage.inc';
 ?>
 <h1>How to order the New York Times</h1>
-<p>To order the New York Times, please click on the following PDF document link below to view the
-   order form. In order to view this document, you must have a PDF reader installed on you machine.
-   If you need to download a PDF reader, please
+<p>Should you choose to obtain your own personal subscription to the New York
+   Times, please click on the following PDF document link to view and print the
+   order form.  In order to view this document, you must have a PDF reader
+   installed on your computer.  If you need to download a PDF reader, please
    <a href="http://www.foxitsoftware.com/pdf/rd_intro.php">click here</a>
    and follow the on screen instructions for downloading and installing Foxit Reader.</p>
 <center><a href="nytimes/order_form/current.pdf">New York Times Order Form</a></center>
-<?
-include 'nytimes/usage.inc';
-bottom();
-?>
+<? bottom(); ?>
