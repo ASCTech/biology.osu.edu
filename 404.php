fb00897a25e824033ad80a4831f50a38a3382f77
Michael Gee <gee.24@osu.edu>
2008-03-26 02:25:24 +0000

    custom 404 message

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?
+require_once 'lib/basics.inc';
+top();
+no_menu();
+?>
+
+<h1 style="text-align:left;margin:1em 0">The page you are looking for has moved or is outdated.</h1>
+
+<p>You could try <a href="<?= $base ?>">returning to the Center for Life Sciences Education's home page</a>
+   or use the menu above to find the information you are looking for.</p> 
+
+<p>Or, if you believe that the page you requested is missing in error,
+   <a href="contact/webmaster.php">please let us know</a>.</p>
+
+<? bottom(); ?>
