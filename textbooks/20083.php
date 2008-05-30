674392259cc1c29c7eb9454dbe3559e784ccf9df
Michael Gee <gee.24@osu.edu>
2008-05-30 13:09:29 +0000

    summer textbooks

---
+++ textbooks/20083.php
@@ -0,0 +1,108 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Textbooks','Spring 2008');
+menu('textbooks');
+?>
+<table class="textbooks" cellspacing="0" cellpadding="0">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Baker</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>101 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2339-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>101N</td>
+  <td>Chordas</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>101 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2339-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>102</td>
+  <td>Taylor</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>102N</td>
+  <td>Santangelo</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>113</td>
+  <td>Cogan &amp; Breitenberger</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp; Reece</td>
+  <td class="isbn">978-0-3215-4325-7</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>113/115 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2355-7</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>114</td>
+  <td>Kovach</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp; Reece</td>
+  <td class="isbn">978-0-3215-4325-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>114 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2349-6</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td />
+  <td />
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
