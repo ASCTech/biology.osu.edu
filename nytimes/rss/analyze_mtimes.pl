7534f73d7a4df48d05bf164362206842db1afb20
biology on excelsior <biology@osu.edu>
2008-06-06 14:16:05 +0000

    Old mtimes are now put aside after processing once.

--- nytimes/rss/analyze_mtimes.pl
+++ nytimes/rss/analyze_mtimes.pl
@@ -9,12 +9,19 @@ use Fatal qw( open );
 
 
 my $mtimes_file = 'mtimes.log';
+my $old_mtimes_file = 'mtimes.log.old';
 
-open(my $fh, '<', $mtimes_file);
-while (my $time = <$fh>) {
+open(my $in_fh,  '<',  $mtimes_file);
+open(my $out_fh, '>>', $old_mtimes_file);
+while (my $time = <$in_fh>) {
+  print {$out_fh} $time;
   my $dt = DateTime->from_epoch(epoch => $time);
   next if 4 < $dt->min and $dt->min < 10;
   print $dt->ymd, "\t", $dt->hms, "\n";
 #  print $dt, '  ', ((4 < $dt->min and $dt->min < 10) ? 'ok' : 'bad'), "\n";
 }
-close($fh);
+close($in_fh);
+close($out_fh);
+
+# perl's truncate doesn't seem to work on this system
+do {open(my $fh, '>', $mtimes_file); close($fh);}
