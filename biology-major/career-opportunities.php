29eb1185632c73a7d0d41643d373949de97c9962
Michael Gee <gee.24@osu.edu>
2008-07-18 19:04:00 +0000

    updated contact info for ASC Career Services

--- biology-major/career-opportunities.php
+++ biology-major/career-opportunities.php
@@ -5,8 +5,8 @@ top('Biology Major','Career Opportunities');
 menu('biology-major');
 ?>
 
-<p>Some areas of biology are currently acknowledged to be hot areas in
-   science. They are:</p>
+<p>Some areas of biology are currently acknowledged to be hot areas in science.
+   They are:</p>
 <ul>
  <li>genetics</li>
  <li>molecular and cell biology</li>
@@ -14,37 +14,35 @@ menu('biology-major');
  <li>medical research</li>
 </ul>
 
-<p>Biology students often continue in careers such as
-   research scientist or college professor, perhaps in one of these hot
-   fields. For these careers, students must first obtain a Ph.D.
-   degree. More often, a biology student goes on to a career in
-   medicine or some other health science. In the private sector, a
-   master's degree may be sufficient for a research or product
+<p>Biology students often continue in careers such as research scientist or
+   college professor, perhaps in one of these hot fields. For these careers,
+   students must first obtain a Ph.D. degree. More often, a biology student goes
+   on to a career in medicine or some other health science. In the private
+   sector, a master's degree may be sufficient for a research or product
    development position.</p>
 
-<p>Those students who are not looking for a career in research or a
-   health profession may seek to move into other science careers such
-   as communications, business (pharmaceutical sales), school teaching,
-   or a myriad of jobs (e.g., in nature centers or as a government
-   biologist or science writer). Some biology majors are eager to go
-   out and find a job in the science arena before continuing graduate
-   work for a master's or Ph.D. degree. The positions that these
-   particular students find are usually those of research assistant,
-   field assistant, or laboratory assistant. Such a student should
-   visit the <a href="http://career.asc.ohio-state.edu/">Arts and
-   Sciences Career Services Office</a> in 05 Brown Hall long before
+<p>Those students who are not looking for a career in research or a health
+   profession may seek to move into other science careers such as
+   communications, business (pharmaceutical sales), school teaching, or a myriad
+   of jobs (e.g., in nature centers or as a government biologist or science
+   writer). Some biology majors are eager to go out and find a job in the
+   science arena before continuing graduate work for a master's or Ph.D. degree.
+   The positions that these particular students find are usually those of
+   research assistant, field assistant, or laboratory assistant. Such a student
+   should visit the <a href="http://asccareerservices.osu.edu/">Arts and
+   Sciences Career Services Office</a> in 48 Townshend Hall long before
    commencement day to perfect job seeking skills.</p>
 
-<p>Whether the biology graduate has a Ph.D., master's, or a bachelor's
-   degree in biology, if work is found in the field of biology, it is
-   most likely going to be found in the key economic industries of
-   health, food, environment, pharmaceuticals, or education.</p>
+<p>Whether the biology graduate has a Ph.D., master's, or a bachelor's degree in
+   biology, if work is found in the field of biology, it is most likely going to
+   be found in the key economic industries of health, food, environment,
+   pharmaceuticals, or education.</p>
 
 <p>In general, jobs for graduates with master's degrees involve more
    responsibility and higher pay. For positions designing or directing
    research projects, a Ph.D. is a must.</p>
 
-<p>Also, please visit the <a href="http://career.asc.ohio-state.edu/">Arts
-   and Sciences Career Services Office</a>, 05 Brown Hall, (614) 292-7055.</p>
+<p>Also, please visit the <a href="http://asccareerservices.osu.edu/">Arts and
+   Sciences Career Services Office</a>, 48 Townshend Hall, (614) 292-7055.</p>
 
 <? bottom(); ?>
