173513dbc9844351c13af40d39dfc22d9b7553f0
Michael Gee <gee.24@osu.edu>
2008-08-20 13:12:24 +0000

    updated more biology advisor info

--- biology-major/requirements.php
+++ biology-major/requirements.php
@@ -5,15 +5,15 @@ top('Biology Major','Course Requirements');
 menu('biology-major');
 ?>
 
-<p>The biology major provides a structured program that includes the
-   major areas of importance to modern biology, as well as an in-depth
-   concentration of study in one of the six departments in the College
-   of Biological Sciences.</p>
+<p>The biology major provides a structured program that includes the major areas
+   of importance to modern biology, as well as an in-depth concentration of
+   study in one of the six departments in the College of Biological
+   Sciences.</p>
 
-<p>The requirements listed here are for individuals seeking a Bachelor
-   of Science degree. A Bachelor of Arts degree is also available. That
-   program follows the same requirements, except for Math 152, which is
-   not required.</p>
+<p>The requirements listed here are for individuals seeking a Bachelor of
+   Science degree. A Bachelor of Arts degree is also available. That program
+   follows the same requirements, except for Math 152, which is not
+   required.</p>
 
 <h2>Part A. Required Prerequisites or Supplements to the Major</h2>
 <span class="detail">(Do not count toward the 45 hour major)</span>
@@ -72,22 +72,22 @@ menu('biology-major');
 <h2>Part C. Electives within the Major</h2>
 
 <ul>
- <li>At least three additional courses at the 300 level or above from
-     one biological science department.</li>
+ <li>At least three additional courses at the 300 level or above from one
+     biological science department.</li>
  <li>At least two of the three courses must be lecture-based courses.</li>
- <li>When courses are taken as a sequence to satisfy a Core requirement
-     (part B), all but one of the courses in the sequence may also be
-	 used to satisfy the three additional courses requirement.</li>
- <li>Independent study is strongly recommended (693 or H783), with up
-     to five credit hours counting towards the 45 hour major.</li>
+ <li>When courses are taken as a sequence to satisfy a Core requirement (part
+     B), all but one of the courses in the sequence may also be used to satisfy
+     the three additional courses requirement.</li>
+ <li>Independent study is strongly recommended (693 or H783), with up to five
+     credit hours counting towards the 45 hour major.</li>
 </ul>
 
 
 <h2>Requirements &ndash; Parts B and C together</h2>
 
 <ul>
- <li>A minimum grade of &quot;C&ndash;&quot; in each course and a 2.0
-     overall GPA in the major.</li>
+ <li>A minimum grade of &quot;C&ndash;&quot; in each course and a 2.0 overall
+     GPA in the major.</li>
  <li>At least three courses must have a laboratory.</li>
  <li>Total: 45 or more hours at the 300 level or above.</li>
  <li>COURSES IN THE MAJOR MUST BE APPROVED BY YOUR BIOLOGY ADVISER.</li>
@@ -99,20 +99,29 @@ menu('biology-major');
 
 <h2>For more information about the biology major, contact:</h2>
 
-<h3>Advisors</h3>
+<p><a href="mailto:biologyadvisor@osu.edu">biologyadvisor@osu.edu</a>, or
+   call (614) 292-1704 for an appointment.</p>
+
+<h3>Undergraduate Research Coordinator</h3>
 
 <ul>
- <li>Peggy Strow, <a href="mailto:Peggy Strow &lt;strow.1@osu.edu&gt;">strow.1@osu.edu</a></li>
- <li>David Wells, <a href="mailto:David Wells &lt;wells.252@osu.edu&gt;">wells.252@osu.edu</a></li>
+ <li>Prof. David Stetson,
+     <a href="mailto:David Stetson &lt;stetson.1@osu.edu&gt;">stetson.1@osu.edu</a>,
+     (614) 292-5307</li>
 </ul>
-
-<p>For an appointment, please call (614) 292-1704.</p>
 
 <h3>Honors Advisors</h3>
 
 <ul>
- <li>Prof. David Stetson, <a href="mailto:David Stetson &lt;stetson.1@osu.edu&gt;">stetson.1@osu.edu</a>, (614) 292-5307</li>
- <li>Prof. Tom Wilson, <a href="mailto:Tom Wilson &lt;wilson.1457@osu.edu&gt;">wilson.1457@osu.edu</a>, (614) 247-6036</li>
+ <li>Prof. W. Mitch Masters,
+     <a href="mailto:W. Mitch Masters &lt;masters.2@osu.edu&gt;">masters.2@osu.edu</a>,
+     (614) 292-4602</li>
+ <li>Prof. David Stetson,
+     <a href="mailto:David Stetson &lt;stetson.1@osu.edu&gt;">stetson.1@osu.edu</a>,
+     (614) 292-5307</li>
+ <li>Prof. Tom Wilson,
+     <a href="mailto:Tom Wilson &lt;wilson.1457@osu.edu&gt;">wilson.1457@osu.edu</a>,
+     (614) 247-6036</li>
 </ul>
 
 <? bottom(); ?>
