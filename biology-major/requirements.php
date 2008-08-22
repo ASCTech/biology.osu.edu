a12c65e999ced0cc8be6d4c9984330f002309f90
Michael Gee <gee.24@osu.edu>
2008-08-22 02:38:50 +0000

    updated the biology major

--- biology-major/requirements.php
+++ biology-major/requirements.php
@@ -5,92 +5,91 @@ top('Biology Major','Course Requirements');
 menu('biology-major');
 ?>
 
-<p>The biology major provides a structured program that includes the major areas
-   of importance to modern biology, as well as an in-depth concentration of
-   study in one of the six departments in the College of Biological
-   Sciences.</p>
+<div class="preface">This revised version of the biology major is effective
+   for all students choosing the biology major on or after June 1, 2008, as
+   well as students selecting the major before that date who choose to switch
+   to the Revised major. Students who choose to remain under the old major may
+   request the descriptive sheet from their advisor.</div>
 
-<p>The requirements listed here are for individuals seeking a Bachelor of
-   Science degree. A Bachelor of Arts degree is also available. That program
-   follows the same requirements, except for Math 152, which is not
-   required.</p>
+<p>The biology major provides a survey of the essential areas of study in
+   modern biology, an individually-tailored focus of study, and an emphasis on
+   methods of communication in the discipline.</p>
 
-<h2>Part A. Required Prerequisites or Supplements to the Major</h2>
-<span class="detail">(Do not count toward the 45 hour major)</span>
+<table class="courses">
+<tbody>
+<tr><th colspan="2"><h2>Part A. Required Prerequisites or Supplements to the Major</h2></th></tr>
+<tr><td colspan="2" class="detail">(Do not count toward the 45 hour major)</td></tr>
 
+<tr><td></td><th>Courses</th><th class="hours">Hours</th></tr>
+
+<tr><td class="bullet">&bull;</td>
+    <td>Biology 113 or H115, 114 or H116</td>
+    <td class="hours">10</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Mathematics 148, 150, 151, 152 or 161 (or Honors versions)</td>
+    <td class="hours">5 &ndash; 19</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Chemistry 121 or H201, 122 or H202, 123 or H203</td>
+    <td class="hours">15</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Chemistry 251, 252, 254 or 245, 255 or 246 (or Honors versions)</td>
+    <td class="hours">10 &ndash; 12</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Physics 111 or 131, 112 or 132, 113 or 133 (or Honors versions)</td>
+    <td class="hours">15</td></tr>
+
+<tr><th colspan="2"><h2>Part B. Core Requirements</h2></th></tr>
+
+<tr><td class="bullet">&bull;</td>
+    <td>Biology 401 &mdash; Integrated Biology
+        <span class="roman">I</span></td>
+    <td class="hours">5</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Biology 402 &mdash; Integrated Biology
+        <span class="roman">II</span></td>
+    <td class="hours">5</td></tr>
+<tr><td class="bullet">&bull;</td>
+    <td>Biology&nbsp;320 &mdash; Biological&nbsp;Inquiry
+        (Sophomore&nbsp;Colloquium)</td>
+    <td class="hours">2</td></tr>
+
+</tbody>
+</table>
+
+<h2>Part C. Specialization Areas</h2>
+<div class="detail">(Individually-designed areas of further study totaling at
+   least 15 hours)</div>
+
+<p>In consultation with his or her advisor, each student must decide on an
+   area of specialization within the broader field of biology. If the
+   pre-approved specializations do not meet a student's needs, he or she may
+   work with a biology advisor to choose a series of at least three advanced
+   courses (300 and above, not including 591 and 597) in the biological
+   sciences with a coherent theme, and present the proposed specialization to
+   the College of Biological Sciences Curriculum Committee for approval.</p>
+
+<h3>Sample Specialization Areas for the Biology Major:</h3>
 <ul>
- <li>Biology 113 or H115, 114 or H116</li>
- <li>Mathematics 148, 150, 151, 152 or 161</li>
- <li>Chemistry 121 or H201, 122 or H202, 123 or H203</li>
- <li>Chemistry 251, 252, 254 or 245, 255 or 246</li>
- <li>Physics 111 or 131, 112 or 132, 113 or 133</li>
+  <li>Education in Life Sciences</li>
+  <li>Forensic Biology</li> 
+  <li>Pre-health Professions</li>
 </ul>
+<p>Please see a biology advisor for the lists of approved courses in these and
+   other specializations.</p>
 
-<h2>Part B. Core Requirements</h2>
-
-<p>Choose one course from each of the five groups below.</p>
-
-<ol>
- <li>Molecular (choose one):
-  <ul>
-   <li>Biochemistry 511, or 613 and 614</li>
-  </ul>
- </li>
- <li>Genetic (choose one):
-  <ul>
-   <li>Microbiology 581</li>
-   <li>Molecular Genetics 500, or 605 and 606</li>
-  </ul>
- </li>
- <li>Cellular (choose one):
-  <ul>
-   <li>EEOB 415 or 630</li>
-   <li>Microbiology 509, or 520 and 521</li>
-   <li>Molecular Genetics 602 or 607</li>
-   <li>Plant Biology 648</li>
-  </ul>
- </li>
- <li>Organismal (choose one):
-  <ul>
-   <li>EEOB 410, or 405.01 and 405.02</li>
-   <li>Entomology 500 or 611F</li>
-   <li>Microbiology 661</li>
-   <li>Plant Biology 436, or 630 and 631</li>
-  </ul>
- </li>
- <li>Ecology/Evolution (choose one):
-  <ul>
-   <li>EEOB 370 or 400 or 413.01 or 413.03</li>
-   <li>Entomology H444 or 641</li>
-   <li>Microbiology 664 or 665</li>
-   <li>Molecular Genetics 640</li>
-  </ul>
- </li>
-</ol>
-
-
-<h2>Part C. Electives within the Major</h2>
-
+<h2>Part D. General Requirements for the Biology Major</h2>
 <ul>
- <li>At least three additional courses at the 300 level or above from one
-     biological science department.</li>
- <li>At least two of the three courses must be lecture-based courses.</li>
- <li>When courses are taken as a sequence to satisfy a Core requirement (part
-     B), all but one of the courses in the sequence may also be used to satisfy
-     the three additional courses requirement.</li>
- <li>Independent study is strongly recommended (693 or H783), with up to five
-     credit hours counting towards the 45 hour major.</li>
-</ul>
-
-
-<h2>Requirements &ndash; Parts B and C together</h2>
-
-<ul>
- <li>A minimum grade of &quot;C&ndash;&quot; in each course and a 2.0 overall
-     GPA in the major.</li>
- <li>At least three courses must have a laboratory.</li>
- <li>Total: 45 or more hours at the 300 level or above.</li>
- <li>COURSES IN THE MAJOR MUST BE APPROVED BY YOUR BIOLOGY ADVISER.</li>
+  <li>45 or more credit hours beyond the prerequisites to the major (some
+      students may need additional courses at the 300-level or above after
+      completing the Core and Specialization Area)</li>
+  <li>Three courses in the major must have a laboratory or data analysis
+      component.</li>
+  <li>Independent Study, e.g. Biology 699 or H783, can be included to a
+      maximum of 5 hours, and may be counted towards the laboratory/data
+      analysis component.</li>
+  <li>A minimum of C&ndash; in each course in the major</li>
+  <li>An overall GPA of at least 2.0 in the major</li>
+  <li>COURSES IN THE MAJOR MUST BE APPROVED BY A BIOLOGY ADVISOR.</li>
 </ul>
 
 
--- biology-major/sample-curriculum.php
+++ biology-major/sample-curriculum.php
@@ -5,7 +5,11 @@ top('Biology Major','Sample Curriculum');
 menu('biology-major');
 ?>
 
-<table id="curriculum" border="0" cellpadding="0" cellspacing="0">
+<table id="curriculum"><tbody>
+
+<tr><td colspan="3" class="preface">The courses listed here apply to the Revised Major, which
+    became effective June 1, 2008.</td></tr>
+<tr class="vspace"><td></td></tr>
 
 <tr><th colspan="3">Freshman Year</th></tr>
 
@@ -17,23 +21,23 @@ menu('biology-major');
  <li>Biological Sciences 100</li>
  <li>Chemistry 121</li>
  <li>Mathematics 150</li>
- <li>GEC &ndash; Foreign Language</li>
+ <li>GEC &ndash; Social Science</li>
 </ul>
 </td>
 
 <td>
 <h3>Winter</h3>
 <ul>
+ <li>Biology 113</li>
  <li>Chemistry 122</li>
  <li>Mathematics 151</li>
- <li>GEC &ndash; Foreign&nbsp;Language</li>
 </ul>
 </td>
 
 <td>
 <h3>Spring</h3>
 <ul>
- <li>Biology 113</li>
+ <li>Biology 114</li>
  <li>Chemistry 123</li>
  <li>Mathematics 152</li>
  <li>GEC &ndash; English&nbsp;110</li>
@@ -50,17 +54,17 @@ menu('biology-major');
 <td>
 <h3>Autumn</h3>
 <ul>
- <li>Biology 114</li>
+ <li>Biology 320 (Major Course)</li>
  <li>Chemistry 251</li>
  <li>GEC &ndash; Foreign&nbsp;Language</li>
- <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities &ndash;&nbsp;Visual/Performing&nbsp;Arts</li>
+ <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities</li>
 </ul>
 </td>
 
 <td>
 <h3>Winter</h3>
 <ul>
- <li>Major Course (EEOB&nbsp;400&nbsp;or&nbsp;413)</li>
+ <li>Biology 401 (Major Course)</li>
  <li>Chemistry 252</li>
  <li>Chemistry 254&nbsp;or&nbsp;245</li>
  <li>GEC &ndash;&nbsp;Foreign&nbsp;Language</li>
@@ -70,10 +74,10 @@ menu('biology-major');
 <td>
 <h3>Spring</h3>
 <ul>
- <li>Major Course (Biochem&nbsp;511)</li>
+ <li>Biology 402 (Major Course)</li>
  <li>Chemistry 255&nbsp;or&nbsp;246</li>
- <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities &ndash;&nbsp;Literature</li>
- <li>GEC &ndash;&nbsp;Social&nbsp;Science</li>
+ <li>GEC &ndash;&nbsp;Foreign&nbsp;Language</li>
+ <li>Elective</li>
 </ul>
 </td>
 
@@ -87,9 +91,9 @@ menu('biology-major');
 <td>
 <h3>Autumn</h3>
 <ul>
- <li>Major Course (Mol&nbsp;Gen&nbsp;500)</li>
+ <li>Major Course</li>
  <li>Physics 111&nbsp;or&nbsp;131</li>
- <li>Elective</li>
+ <li>GEC &ndash;&nbsp;Foreign&nbsp;Language</li>
 </ul>
 </td>
 
@@ -99,7 +103,6 @@ menu('biology-major');
  <li>Major Course</li>
  <li>Physics 112&nbsp;or&nbsp;132</li>
  <li>GEC &ndash;&nbsp;Social&nbsp;Science</li>
- <li>Elective</li>
 </ul>
 </td>
 
@@ -122,19 +125,16 @@ menu('biology-major');
 <td>
 <h2>Autumn</h2>
 <ul>
- <li>Major Course</li>
- <li>GEC &ndash;&nbsp;Social&nbsp;Science</li>
- <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities &ndash;&nbsp;History&nbsp;I</li>
- <li>Elective</li>
+ <li>Major Courses</li>
+ <li>GEC &ndash;&nbsp;History</li>
 </ul>
 </td>
 
 <td>
 <h3>Winter</h3>
 <ul>
- <li>Major Course</li>
- <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities &ndash;&nbsp;History&nbsp;II</li>
- <li>Major Course</li>
+ <li>Major Courses</li>
+ <li>GEC &ndash;&nbsp;History</li>
  <li>Elective</li>
 </ul>
 </td>
@@ -143,13 +143,12 @@ menu('biology-major');
 <h3>Spring</h3>
 <ul>
  <li>Major Course</li>
- <li>Elective</li>
- <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities &ndash;&nbsp;Cultures&nbsp;&amp;&nbsp;Ideas</li>
+ <li>GEC &ndash;&nbsp;Arts&nbsp;&amp;&nbsp;Humanities</li>
 </ul>
 </td>
 
 </tr>
 
-</table>
+</tbody></table>
 
 <? bottom(); ?>
