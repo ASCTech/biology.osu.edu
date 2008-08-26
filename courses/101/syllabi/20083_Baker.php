bff8dcd58a7ccdab9b125d52c8a72264402a5261
Michael Gee <gee.24@osu.edu>
2008-08-26 15:36:00 +0000

    I'm about to throw away a lot of manual effort I put into 20083_Baker, so I want to commit the file even though it is incomplete.

---
+++ courses/101/syllabi/20083_Baker.php
@@ -0,0 +1,197 @@
+<?
+chdir('../../..');
+require_once 'lib/basics.inc';
+top('Biology 101','Syllabus');
+menu('101');
+?>
+<h1>Biology 101 &ndash; An Introduction to Biology</h1>
+
+<p style="margin-top:2em;text-align:center">SU 2008 &ndash; 5 credit hrs &ndash; Lecture: Tue, Wed, Thur in JE 001<br />
+    Prerequisite: Math 075 or 076 or 104 or Mathematics Placement Level R</p>
+
+<table style="margin-top:2em;width:93%"><tbody>
+<tr>
+  <th style="width:30%;text-align:left">Instructor:</th>
+  <th style="width:30%;text-align:left">Course Coordinator:</th>
+  <th style="width:30%;text-align:left">Assistant Course Coordinator:</th>
+</tr>
+<tr>
+  <td style="padding:0 1em">Dr. Neil R. Baker<br/>541A Biological Sciences Building<br/>baker.2@osu.edu &ndash; preferred<br/>292&ndash;3342</td>
+  <td style="padding:0 1em">Adam Andrews<br/>255B Jennings Hall<br/>andrews.171@osu.edu<br/>247&ndash;6345</td>
+  <td style="padding:0 1em">Amy Kulesza<br/>255D Jennings Hall<br/>kulesza.5@osu.edu<br/>247&ndash;0029</td>
+</tr>
+</tbody></table>
+<table style="margin-top:1em"><tbody>
+<tr>
+  <th style="vertical-align:middle">Head TA:</th>
+  <td style="vertical-align:middle">Daniel Lima &ndash; lima.19@osu.edu</td>
+</tr>
+</tbody></table>
+
+<h2>Text/Materials</h2>
+<p><em>Biology: Concepts and Applications (7<sup>th</sup> edition)</em> by Starr, &copy; 2008, (available at all bookstores) &ndash; ISBN 0495119814</p>
+<p>Biology 101 Lab Manual 2007&ndash;2008, The Ohio State University</p>
+<p><em>New York Times</em>: student subscription information available on Carmen (carmen.osu.edu)</p>
+
+<h2>Goals</h2>
+<p>The major goal of this course is to provide you with a foundation of
+   principles that are central to biology. Through the study of these core
+   concepts the nature of scientific inquiry will be reinforced. These goals
+   are in-line with the goals and objectives of the GEC Natural Science
+   category. (outlined below)</p>
+
+<h2>Learning Outcomes</h2>
+<p>You will achieve a sound understanding of core concepts in biology. Some
+   new terms and ideas will be introduced but as much as possible you will not
+   be expected to memorize facts and figures. The information presented will
+   also prepare you to make sound life choices related to biology, e.g. Are
+   genetically modified foods safe to eat? This course is designed to provide
+   you with the foundation of knowledge to discuss and evaluate such topics,
+   which will be the focus of Biology 102. Thus Biology 101 will provide the
+   framework of knowledge you need for life science future courses and biology
+   related decisions. Specifically:</p>
+<ul>
+  <li>Students will recall current and historical aspects of energetics,
+      genetics, evolution, and ecology. Students will describe biological
+      processes related to energetics, genetics, evolution, and ecology.
+      Students will analyze the current and future significance of energetics,
+      genetics, evolution, and ecology on society.</li>
+  <li>Students will apply skills that demonstrate their scientific literacy by
+      communicating about the content and validity of articles related to
+      science in the popular press.</li>
+  <li>Students will value the study of biology.</li>
+  <li>Students will demonstrate an understanding of the nature of science.
+      This includes (1) the way that scientist develop and evaluate
+      explanations of natural phenomena using criteria fundamental to
+      scientific inquiry and (2) the understanding that science is a human
+      endeavor.</li>
+  <li>Students will work productively and effectively in a group.</li>
+</ul>
+
+<? require 'courses/GEC.inc'; ?>
+
+<p>In Biology 101, non-major Biology students meet the GEC Natural Science
+   Learning Objectives in multiple ways. Students gain an understanding of the
+   foundations of modern biology by studying organismal diversity, evolution,
+   ecology, energetics, genetics, reproduction, and cell structure and
+   function. The students will perform inquiry-based laboratory activities
+   that provide insight into scientific methods and habits of mind. Lectures
+   and labs will include references to the development of scientific concepts
+   to help students understand the history and nature of science. The
+   interactions among science, technology, and society are interwoven
+   throughout the course, and assignments give students opportunities to
+   personally consider the interactions. Biology 101 is designed to help
+   prepare students to make intelligent, informed decisions on the biological
+   and technological decisions that they will face in life.</p>
+
+<h2>Structure</h2>
+<p>The class will meet 3 times per week for 1 hour and 48 minutes each session
+   for lecture. The content of the lecture is determined by the laboratory
+   activity that week. Lecture time will be used to reinforce the concepts
+   that were introduced in lab and to expand upon those concepts. A mixture of
+   lecture and group discussion/activity formats will be used. Participation
+   by all members of the group in the activities is expected. In addition to
+   lecture notes you will be asked to respond to specific questions or
+   problems. Most of these will be group activities, but a few will require
+   individual responses. These will be collected and graded.</p>
+<p>Laboratories meet twice weekly for approximately 3 hrs. The lab activities
+   are central to the course, as lecture will generally be used to explain and
+   discuss the preceding lab. Each laboratory period includes a recitation
+   period during which questions can be answered and concepts from the lab can
+   be reviewed. Review your lab manual for lab policies and procedures.</p>
+
+<h2>Materials</h2>
+<p>The text will be used to provide background information and to clarify
+   points made during lecture.  I recommend it highly as my lectures follow it
+   fairly closely.  The free On-line text – Kimball’s Biology Pages can be
+   used in place of the text.  It is excellent but does go into most topics in
+   more depth than I will. Using it as a resource to supplement the lectures
+   is the best approach. Additional handouts and web sites will be provided as
+   needed.</p>
+<p>Carmen is being used for this course to organize course material including
+   grades, course syllabus, assignments and the pretest.  Before you can use
+   Carmen you must activate your OSU email account. The URL for Carmen is
+   http://carmen.osu.edu/ and Biology 101 should be listed under My Courses on
+   your Carmen homepage.  The username is your OSU name.# and the password is
+   the one you use with all OSU email and registration systems. If you have a
+   problem logging in or using Carmen contact 688-HELP or carmen@osu.edu.</p>
+<p><strong>IMPORTANT: The CLSE and its course staff will only send email to
+   your official OSU email account.</strong></p>
+
+<h2>Absences</h2>
+<p>I consider the following my responsibilities to you:</p>
+<ul>
+ <li>I accept joint responsibility with you for your learning.</li>
+ <li>I will emphasize thinking processes in addition to products; I will use homework, readings, quizzes and other opportunities to help you organize
+     the content component of this course.</li>
+ <li>I will present some of the best examples of the nature of science as demonstrated within biology.</li>
+ <li>I will consider and value your feedback in this course.</li>
+ <li>I will model patience throughout.</li>
+ <li>I hope to make this an interesting and exciting course.</li>
+ <li>I recognize that this is a large and, at least potentially, very impersonal course. I will do everything I know to minimize this possibility
+     including cooperative learning opportunities in lecture and laboratory, maintained office hours, open-discussion office hours when possible,
+     visits to the labs. I encourage you to contact me whenever necessary and would especially appreciate e-mailed questions about material we are
+     covering (get the e-mails to me by an hour before lecture, and I will try to incorporate answers into the next lecture).</li>
+ <li>I will arrive in the lecture hall before class starts and will leave only when class is over. I will do nothing to distract others from the
+     presentation of the course during lecture or lab.</li>
+</ul>
+<p>By enrolling in this course, I assume you accept the following responsibilities. If you cannot accept these, please drop this class &mdash; there is
+   a waiting list to get in.</p>
+<ul>
+ <li>You accept joint responsibility with me for your learning.</li>
+ <li>Given my attempt to balance process with product, you will work to understand all exercises (e.g. text readings) designed to help you organize the
+     content component of this course.</li>
+ <li>You will demonstrate patience throughout.</li>
+ <li>You recognize that this is a large and, at least potentially, very impersonal course. You will do everything you know to minimize this possibility
+     including participating effectively in cooperative learning opportunities in lecture and laboratory and participating in opportunities to contact
+     me whenever needed. In particular, you will show respect to fellow students by participating in lecture and laboratory exercises without being
+     disruptive.</li>
+ <li>You will arrive in the lecture hall before class starts and will leave only when class is over. You will do nothing to distract others from the
+     presentation of the course during lecture or lab.</li>
+</ul>
+
+<h2>Grading:</h2>
+<p>Lecture Grade (70% of final grade): Each of seven weekly quizzes will be worth 10 lecture points; the final exam will be worth 30 points. There are
+   a total number of 100 lecture points.</p>
+<p>Lab Grade (30% of final grade): Each laboratory exercise will be worth 100 lab points; the New York Times clippings project 50 lab points; teaching
+   assistant discretionary points will be worth 50 points. There are 1000 total lab points.</p>
+<p>Final Grade: Your final grade will be the sum of your total lecture points accumulated divided by 100 &times; 70%, and your total laboratory points
+   accumulated divided by 1000 &times; 30%.</p>
+<h3>Grades will be calculated on the basis of the above sum using the following formula guidelines:</h3>
+<table id="gradescale">
+ <tr><th>Percent</th><th>Grade</th></tr>
+ <tr><td>93 &ndash; 100</td><td>&nbsp;&nbsp;&nbsp;&nbsp;A</td></tr>
+ <tr><td>90 &ndash;  92</td><td>&nbsp;&nbsp;&nbsp;&nbsp;A&minus;</td></tr>
+ <tr><td>87 &ndash;  89</td><td>&nbsp;&nbsp;&nbsp;&nbsp;B+</td></tr>
+ <tr><td>83 &ndash;  86</td><td>&nbsp;&nbsp;&nbsp;&nbsp;B</td></tr>
+ <tr><td>80 &ndash;  82</td><td>&nbsp;&nbsp;&nbsp;&nbsp;B&minus;</td></tr>
+ <tr><td>77 &ndash;  79</td><td>&nbsp;&nbsp;&nbsp;&nbsp;C+</td></tr>
+ <tr><td>73 &ndash;  76</td><td>&nbsp;&nbsp;&nbsp;&nbsp;C</td></tr>
+ <tr><td>70 &ndash;  72</td><td>&nbsp;&nbsp;&nbsp;&nbsp;C&minus;</td></tr>
+ <tr><td>67 &ndash;  69</td><td>&nbsp;&nbsp;&nbsp;&nbsp;D+</td></tr>
+ <tr><td>63 &ndash;  67</td><td>&nbsp;&nbsp;&nbsp;&nbsp;D</td></tr>
+ <tr><td>0  &ndash;  62</td><td>&nbsp;&nbsp;&nbsp;&nbsp;E</td></tr>
+</table>
+<p>NOTE: I reserve the option to curve the final distribution of grades up; I will not curve them down.</p>
+
+<h2>Absences:</h2>
+<p>Missed exams/quizzes should be made up within a week of the normal exercise; if you are ill for an exam/quiz, please contact your teaching assistant
+   as soon as possible (see also FAQ on website). If you miss an exam/quiz for a university sanctioned event, please inform us prior to your absence.
+   Please provide a written excuse (e.g. note from a health care practitioner) for any missed lab/quiz. Late arrivals for quizzes will not be offered a
+   quiz after the first person has handed in a completed quiz. Make-up exams and senior finals will usually be short answer or essay. Please notify the
+   head TA by the seventh week of classes if you need to take a graduating senior final.</p>
+<p>The laboratory section of this class is an integral part of the learning experience; missing three or more labs will result in a grade of incomplete
+   or withdrawal.</p>
+
+<h2>Grading Errors:</h2>
+<p>All grades will be posted on Carmen; you will have 10 class days to challenge any grade, or to inquire about a grade that is not posted. After this
+   the statute of limitations for appeal has expired, and any grade not posted remains a zero.</p>
+
+<h2>New York Times Clippings Project:</h2>
+<p>A major goal of this course is to demonstrate the relevance of biology to society today. Accordingly, you should turn into your laboratory teaching
+   assistant each week an article from the last week of the New York Times with a brief paragraph written explaining the article and how it indicates
+   the relevancy of its biological topic to society today. These will be graded full credit or no credit and are due at the start of each lab period
+   starting with the second lab exercise of the quarter. The article you submit should be clipped from the paper itself, photocopied or downloaded (the
+   New York Times is available in most libraries and online at <a href="http://www.nytimes.com/">www.nytimes.com</a>).</p>
+
+<? bottom(); ?>
