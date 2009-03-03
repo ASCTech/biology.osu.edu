7698609324b8ae60371d633af2d6fb8e4a1b6a7b
Michael Gee <gee.24@osu.edu>
2009-03-03 19:42:28 +0000

    Spring Textbooks

---
+++ textbooks/20092.php
@@ -0,0 +1,136 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Textbooks','Spring 2009');
+menu('textbooks');
+?>
+<table class="textbooks" cellspacing="0" cellpadding="0">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Andrews, Chordas, &amp;&nbsp;Cogan</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>101 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2842-2</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>102</td>
+  <td>Rissing, Taylor, &amp;&nbsp;Hurdzan</td>
+  <td>No Textbook</td>
+  <td/>
+  <td/>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>113</td>
+  <td>Waddell &amp;&nbsp;Swenson</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-321-54325-7</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>113/115 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2855-2</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>114</td>
+  <td>Taylor</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-321-54325-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>114/116 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2350-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td />
+  <td />
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>H115</td>
+  <td>Breitenberger &amp;&nbsp;Stetson</td>
+  <td>Biology</td>
+  <td>Brooker</td>
+  <td class="isbn">978-0-0732-6807-1<br />(eBook:&nbsp;0077281543)</td>
+ </tr>
+ <tr class="gray">
+  <td />
+  <td />
+  <td>113/115 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2855-2</td>
+ </tr>
+ <tr><td>&nbsp;</td></tr>
+ <tr>
+  <td>H116</td>
+  <td>Wilson</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-321-54325-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>114/116 Lab Manual</td>
+  <td />
+  <td class="isbn">978-0-7380-2350-7</td>
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td />
+  <td />
+ </tr>
+ <tr>
+  <td />
+  <td />
+  <td>Natural History of Medicinal Plants</td>
+  <td>Sumner</td>
+  <td>0-88192-483-0</td>
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
