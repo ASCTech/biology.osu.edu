84db9d115354663089c6e15d74873cd209698b8e
Michael Gee <michaelpgee@gmail.com>
2010-02-11 16:23:09 -0500

    starting BioSci day page

---
+++ biosci-day/agenda.php
@@ -0,0 +1,14 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('BioSci Day', 'Agenda');
+menu('biosci-day');
+?>
+
+<h1>Coming Soon!</h1>
+
+<p>Please check back in a few days.  This page should become available soon.</p>
+
+<p>Thanks!</p>
+
+<? bottom(); ?>
