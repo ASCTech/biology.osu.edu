64c4d22a331d0d70991419c0ef9906741bafa152
Michael Gee <michaelpgee@gmail.com>
2010-02-22 14:26:50 -0500

    adding directions and map for biosci day

--- biosci-day/directions.php
+++ biosci-day/directions.php
@@ -5,10 +5,37 @@ top('BioSci Day', 'Directions');
 menu('biosci-day');
 ?>
 
-<h1>Coming Soon!</h1>
+<h1>Directions to Jennings Hall on the Ohio State campus</h1>
 
-<p>Please check back in a few days.  This page should become available soon.</p>
+<h2>From the south</h2>
+<ul>
+<li>Exit Rte 315 at Medical Center Exit</li>
+<li>Turn <em><u>left</u></em> onto Cannon Drive</li>
+<li>Turn <em><u>right</u></em> onto 12th Avenue</li>
+<li>Proceed on 12th Avenue all the way to Neil Avenue; <strong>do not park in the 12th Avenue garage</strong></li>
+<li>Turn left onto Neil; you should see students with signs by now</li>
+<li>Proceed north onto Neil and take the first left you can, just before the getes on Neil</li>
+<li>Follow the drive to the Neil Avenue parking garage</li>
+<li>The Jennings Hall Auditorium is next to the garage, and the student escorts will stand ready to direct you</li>
+</ul>
 
-<p>Thanks!</p>
+<h2>From the north</h2>
+<ul>
+<li>Exit Rte 315 at Lane Avenue</li>
+<li>Turn <em><u>left</u></em> onto Lane Avenue</li>
+<li>At the second intersection, turn <em><u>right</u></em> onto Olentangy River Road</li>
+<li>Take a <em><u>left</u></em> at the first intersection, onto Herrick Drive</li>
+<li>Take a <em><u>right</u></em> at the first intersection, onto Cannon Drive</li>
+<li>Take a <em><u>left</u></em> at the first intersection, onto 12th Avenue</li>
+<li>Proceed on 12th Avenue all the way to Neil Avenue; <strong>do not park in the 12th Avenue garage</strong></li>
+<li>Turn left onto Neil; you should see students with signs by now</li>
+<li>Proceed north onto Neil and take the first left you can, just before the getes on Neil</li>
+<li>Follow the drive to the Neil Avenue parking garage</li>
+<li>The Jennings Hall Auditorium is next to the garage, and the student escorts will stand ready to direct you</li>
+</ul>
+
+<h2>Map</h2>
+
+<img alt="Map of OSU around Jennings Hall" src="images/biosci-day-map.png" style="width:100%"/>
 
 <? bottom(); ?>
