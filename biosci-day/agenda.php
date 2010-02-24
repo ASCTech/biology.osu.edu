182ca7f63ae945c158359fcd808cf195be47397f
Michael Gee <michaelpgee@gmail.com>
2010-02-24 14:32:14 -0500

    added the agenda

--- biosci-day/agenda.php
+++ biosci-day/agenda.php
@@ -5,10 +5,39 @@ top('BioSci Day', 'Agenda');
 menu('biosci-day');
 ?>
 
-<h1>Coming Soon!</h1>
+<style>
+p.head {
+  margin: 1em;
+  text-align: center;
+  font-weight: bold;
+}
+div.centerer, table.centered {
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+table#listing td {
+  padding: 0.5em;
+  text-align: left;
+}
+</style>
 
-<p>Please check back in a few days.  This page should become available soon.</p>
+<h1>BioSci Day &mdash; Agenda</h1>
 
-<p>Thanks!</p>
+<p class="head">March 6, 2010</p>
+<p class="head">All Events will Take Place in Jennings Hall</p>
+<p class="head">Neil Avenue at 12th Avenue</p>
+<p class="head">Ohio State University main campus</p>
+
+<div class="centerer">
+<table class="centered" id="listing">
+<tr><td>9:30 &ndash; 10:30</td><td>Registration</td></tr>
+<tr><td>10:00</td><td>Welcome</td></tr>
+<tr><td>10:15 &ndash; 11:45</td><td>Faculty presentations</td></tr>
+<tr><td>11:45 &ndash; 1:00</td><td>Lunch</td></tr>
+<tr><td>1:00 &ndash; 2:00</td><td>Student Panel</td></tr>
+<tr><td>2:00</td><td>Adjourn</td></tr>
+</table>
+</div>
 
 <? bottom(); ?>
