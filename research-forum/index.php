9af014a600a725a5f13675741d17d98b1f1bd5b2
Michael Gee <gee.24@osu.edu>
2010-03-02 15:12:53 -0500

    Research forum site added

---
+++ research-forum/index.php
@@ -0,0 +1,37 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('BMPS Undergraduate Research Forum');
+menu('research-forum');
+?>
+
+<p>Biological, Mathematical and Physical Sciences announce their 2010 research forum for undergraduate students majoring in or conducting research in <strong>Actuarial Science, Astronomy, Biochemistry, Biology, Chemistry, Computer and Information Science, Evolution and Ecology, Geological Sciences, Mathematics, Microbiology, Molecular Genetics, Physics, Plant Cellular and Molecular Biology, and Zoology.</strong></p>
+
+<p>The Undergraduate Research Forum will be held <strong>Friday, April 23, 2010</strong>, 2–6 p.m., in the atrium of the Physics Research Building, 191 W. Woodruff Avenue.  All students interested in undergraduate research in math and science are welcome to attend.</p>
+
+<img id="research-forum-from-above" src="images/research-forum-from-above.jpg" alt="a view of the research forum from above" />
+
+<div class="schedule">
+<h2>Undergraduate Research Forum schedule:</h2>
+<table>
+<tr><td>2 &ndash; 3 pm</td><th>Students setup posters</th></tr>
+<tr><td>3 &ndash; 5 pm</td><th>Poster viewing &amp; judging</th></tr>
+<tr><td>5 &ndash; 5:45 pm</td><th>Judges confer</th></tr>
+<tr><td>6 pm</td><th>Awarding of prizes</th></tr>
+</table>
+</div>
+
+<p>The forum will provide students the opportunity to present their research to their peers and their professors, polish their presentation skills, and, for many, prepare for the <a href="http://denman.osu.edu">Denman Undergraduate Research Forum</a>.</p>
+
+<h2>Abstracts are due on April 2, 2010.</h2>
+<p>All students submitting abstracts and presenting posters will be awarded $50 gift cards to Barnes &amp; Noble.  Larger prizes will be awarded at the forum for the best posters.</p>
+<p><a href="research-forum/register.php">Register here</a> if you plan to submit an abstract and present a poster.</p>
+
+<div class="contact">
+<h2>Forum organizer and primay contact:</h2>
+<p>Dr. Caroline Breitenberger,<br/>
+Director, Center for Life Sciences Education,<br/>
+(614) 292&ndash;6945,<br/>
+<a href="mailto:Dr. Caroline Breitenberger &lt;breitenberger.1@osu.edu&gt;">breitenberger.1@osu.edu</a></p>
+
+<? bottom(); ?>
---
+++ research-forum/register.php
@@ -0,0 +1,22 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('BMPS Undergraduate Research Forum', 'Registration');
+menu('research-forum');
+?>
+
+<p class="notice">Students who are considering presenting a research poster at this year’s forum should register as soon as possible.</p>
+
+<div class="instructions">
+<h2>Instructions:</h2>
+<ol>
+<li>Log in to <a href="https://carmen.osu.edu" target="_blank">Carmen</a> with your OSU username and password.</li>
+<li>Click on &quot;Join&quot; in the gray bar below the Carmen logo.</li>
+<li>Click on &quot;Join&quot; near the bottom of the next page.</li>
+<li>Find the &quot;BMPS Undergraduate Research Forum&quot; link and click on it.</li>
+<li>Complete the registration form (in addition to your own personal information, we need to have your research advisor's name and email address).</li>
+<li>Abstracts may be uploaded at a later date, closer to the April 2 deadline. We think you will find much useful information to help you prepare your abstract and poster at this web site.</li>
+</ol>
+</div>
+
+<? bottom(); ?>
