84aa8ea9b35ef2804bfad4fded552acb23f98978
Michael Gee <gee.24@osu.edu>
2010-03-16 13:18:48 -0400

    SP10 textbooks

---
+++ textbooks/20102.php
@@ -0,0 +1,203 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Textbooks','Spring 2010');
+menu('textbooks');
+?>
+<table class="textbooks">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Cogan</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3305-1</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF &nbsp;(clicker)</td>
+  <td></td>
+  <td class="isbn notabook">not a book</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>101</td>
+  <td>Chordas</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3305-1</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>101N</td>
+  <td>Taylor</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3305-1</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>102</td>
+  <td>Rissing</td>
+  <td>New York Times only</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>102N</td>
+  <td>Taylor</td>
+  <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition</td>
+  <td>Starr,&nbsp;Cecie</td>
+  <td class="isbn">978-0-4951-1981-4</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>113</td>
+  <td>Stetson</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-8053-6844-4</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>113/115 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3317-4</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>114</td>
+  <td>Cooley</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-8053-6844-4</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>114/116 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3313-6</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>H115</td>
+  <td>Dean</td>
+  <td>Biology, 2nd edition</td>
+  <td>Brooker</td>
+  <td class="isbn">978-0-0773-4996-7</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>113/115 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3317-4</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF &nbsp;(clicker)</td>
+  <td></td>
+  <td class="isbn notabook">not a book</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>H116</td>
+  <td>Wilson</td>
+  <td>Biology, 8th edition</td>
+  <td>Campbell &amp;&nbsp;Reece</td>
+  <td class="isbn">978-0-8053-6844-4</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>114/116 Lab Manual</td>
+  <td></td>
+  <td class="isbn">978-0-7380-3313-6</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Pigs</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>402</td>
+  <td>Breitenberger, Freudstein, &amp;&nbsp;Chordas</td>
+  <td class="notabook">a comprehensive major&#39;s introductory Biology text (e.g.&nbsp;Campbell)</td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Life Sciences Student Lab Notebook</td>
+  <td></td>
+  <td class="isbn">978-1-930882-35-5</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF &nbsp;(clicker)</td>
+  <td></td>
+  <td class="isbn notabook">not a book</td>
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
