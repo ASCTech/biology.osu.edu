14a22d95844bc6f243d5930e583268d9015267fa
Michael Gee <gee.24@osu.edu>
2010-03-17 12:26:28 -0400

    added Issue Resolution

---
+++ courses/issue_resolution.php
@@ -0,0 +1,16 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Biology Courses', 'Issue Resolution');
+menu('101');
+?>
+<h1>Issue Resolution in Biology Courses</h1>
+<p>The Center for Life Sciences Education believes that student concerns are
+   usually most effectively addressed by the staff closest to the situation.
+   Therefore, students are ordinarily expected to address issues or concerns
+   with their TAs first.</p>
+<p>If the issue cannot be resolved by your TA, or for some reason you feel
+   that you absolutely cannot address your concern with your TA, please feel
+   free to contact your Course Coordinator (listed on the syllabus) or
+   Assistant Director Matt Misicka.</p>
+<? bottom(); ?>
