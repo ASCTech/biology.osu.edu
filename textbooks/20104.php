57b5769e630a2ef8840eb699902b729cadb364b1
mikegee <gee.24@osu.edu>
2010-09-14 14:02:00 -0400

    css fix

--- textbooks/20104.php
+++ textbooks/20104.php
@@ -15,15 +15,15 @@ menu('textbooks');
  <tr class="gray">
   <td>101</td>
   <td>Chordas</td>
-  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="notabook">recommended</span></td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
   <td>Phelan</td>
   <td class="isbn">978-1-4292-4666-8<br>
-    <span class="notabook">e-book:</span> 1429277955</td>
+    <span class="subtext">e-book:</span> 1429277955</td>
  </tr>
  <tr class="gray">
   <td></td>
   <td></td>
-  <td>101 Lab Manual <span class="notabook">required</span></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3963-3</td>
  </tr>
@@ -31,15 +31,15 @@ menu('textbooks');
  <tr>
   <td>101</td>
   <td>Cogan</td>
-  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="notabook">recommended</span></td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
   <td>Phelan</td>
   <td class="isbn">978-1-4292-4666-8<br>
-    <span class="notabook">e-book:</span> 1429277955</td>
+    <span class="subtext">e-book:</span> 1429277955</td>
  </tr>
  <tr>
   <td></td>
   <td></td>
-  <td>101 Lab Manual <span class="notabook">required</span></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3963-3</td>
  </tr>
@@ -47,7 +47,7 @@ menu('textbooks');
   <td></td>
   <td></td>
   <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker) 
-      <span class="notabook">required</span></td>
+      <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn notabook">not a book</td>
  </tr>
@@ -55,15 +55,15 @@ menu('textbooks');
  <tr class="gray">
   <td>101</td>
   <td>Pangle</td>
-  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="notabook">recommended</span></td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
   <td>Phelan</td>
   <td class="isbn">978-1-4292-4666-8<br>
-    <span class="notabook">e-book:</span> 1429277955</td>
+    <span class="subtext">e-book:</span> 1429277955</td>
  </tr>
  <tr class="gray">
   <td></td>
   <td></td>
-  <td>101 Lab Manual <span class="notabook">required</span></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3963-3</td>
  </tr>
@@ -71,15 +71,15 @@ menu('textbooks');
  <tr>
   <td>101N</td>
   <td>Taylor</td>
-  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="notabook">recommended</span></td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
   <td>Phelan</td>
   <td class="isbn">978-1-4292-4666-8<br>
-    <span class="notabook">e-book:</span> 1429277955</td>
+    <span class="subtext">e-book:</span> 1429277955</td>
  </tr>
  <tr>
   <td></td>
   <td></td>
-  <td>101 Lab Manual <span class="notabook">required</span></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3963-3</td>
  </tr>
@@ -87,7 +87,7 @@ menu('textbooks');
   <td></td>
   <td></td>
   <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker) 
-      <span class="notabook">required</span></td>
+      <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn notabook">not a book</td>
  </tr>
@@ -96,7 +96,7 @@ menu('textbooks');
   <td>102</td>
   <td>Reeve</td>
   <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition
-      <span class="notabook">recommended</span></td>
+      <span class="subtext">recommended</span></td>
   <td>Starr</td>
   <td class="isbn">978-0-495-11981-4</td>
  </tr>
@@ -105,7 +105,7 @@ menu('textbooks');
   <td>102</td>
   <td>Gordon</td>
   <td>Biology: Concepts&nbsp;&amp;&nbsp;Applications 7th&nbsp;edition
-      <span class="notabook">recommended</span></td>
+      <span class="subtext">recommended</span></td>
   <td>Starr</td>
   <td class="isbn">978-0-495-11981-4</td>
  </tr>
@@ -114,13 +114,13 @@ menu('textbooks');
   <td>113</td>
   <td rowspan="2">Ding &amp;&nbsp;Weinstein</td>
   <td>Mastering&nbsp;Biology, 8th&nbsp;edition
-      <span class="notabook">required</span></td>
+      <span class="subtext">required</span></td>
   <td>Campbell</td>
   <td class="isbn">978-0-321-54325-7</td>
  </tr>
  <tr class="gray">
   <td></td>
-  <td>113/115 Lab Manual <span class="notabook">required</span></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3972-5</td>
  </tr>
@@ -128,14 +128,14 @@ menu('textbooks');
  <tr>
   <td>113</td>
   <td>Rothacker</td>
-  <td>Biology, 8th&nbsp;edition <span class="notabook">required</span></td>
+  <td>Biology, 8th&nbsp;edition <span class="subtext">required</span></td>
   <td>Campbell</td>
   <td class="isbn">978-0-8053-6844-4</td>
  </tr>
  <tr>
   <td></td>
   <td></td>
-  <td>113/115 Lab Manual <span class="notabook">required</span></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3972-5</td>
  </tr>
@@ -143,14 +143,14 @@ menu('textbooks');
  <tr class="gray">
   <td>114</td>
   <td>Taylor</td>
-  <td>Biology, 8th&nbsp;edition <span class="notabook">required</span></td>
+  <td>Biology, 8th&nbsp;edition <span class="subtext">required</span></td>
   <td>Campbell</td>
   <td class="isbn">978-0-8053-6844-4</td>
  </tr>
  <tr class="gray">
   <td></td>
   <td></td>
-  <td>114/116 Lab Manual <span class="notabook">required</span></td>
+  <td>114/116 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3970-1</td>
  </tr>
@@ -158,7 +158,7 @@ menu('textbooks');
   <td></td>
   <td></td>
   <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart
-      <span class="notabook">recommended</span></td>
+      <span class="subtext">recommended</span></td>
   <td></td>
   <td></td>
  </tr>
@@ -166,7 +166,7 @@ menu('textbooks');
   <td></td>
   <td></td>
   <td>Concise&nbsp;Dissection, Pigs
-      <span class="notabook">recommended</span></td>
+      <span class="subtext">recommended</span></td>
   <td></td>
   <td></td>
  </tr>
@@ -174,14 +174,14 @@ menu('textbooks');
  <tr>
   <td>H115</td>
   <td>Stetson</td>
-  <td>Biology, 8th&nbsp;edition <span class="notabook">required</span></td>
+  <td>Biology, 8th&nbsp;edition <span class="subtext">required</span></td>
   <td>Campbell</td>
   <td class="isbn">978-0-8053-6844-4</td>
  </tr>
  <tr>
   <td></td>
   <td></td>
-  <td>113/115 Lab Manual <span class="notabook">required</span></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn">978-0-7380-3972-5</td>
  </tr>
@@ -189,14 +189,14 @@ menu('textbooks');
  <tr class="gray">
   <td>401</td>
   <td rowspan="2">Breitenberger, Ridgway &amp;&nbsp;Klompen</td>
-  <td class="notabook">a comprehensive major&#39;s introductory Biology text (e.g.&nbsp;Campbell) is required</td>
+  <td class="subtext">a comprehensive major&#39;s introductory Biology text (e.g.&nbsp;Campbell) is required</td>
   <td></td>
   <td></td>
  </tr>
  <tr class="gray">
   <td></td>
   <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker) 
-      <span class="notabook">required</span></td>
+      <span class="subtext">required</span></td>
   <td></td>
   <td class="isbn notabook">not a book</td>
  </tr>
