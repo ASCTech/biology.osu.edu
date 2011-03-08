680b5c190cc3aa254c65fee49347b1c54031996f
mikegee <gee.24@osu.edu>
2011-03-08 13:54:44 -0500

    finishing 2011 Researh Forum site

--- research-forum/register.php
+++ research-forum/register.php
@@ -5,6 +5,13 @@ top('NMS Undergraduate Research Forum', 'Registration', 'no-navbar');
 menu('research-forum');
 ?>
 
-<p class="notice">Information about Research Forum registration will be posted by March 1, 2011.</p>
+<ul>
+	<li>To submit your abstract and register to present a research poster at the April 29, 2011 NMS Undergraduate Research Forum, <a href="https://ruby.asc.ohio-state.edu/bio">click here</a>.</li>
+	<li>Information about how to write an abstract is available at the <a href="http://denman.osu.edu/guidlines.aspx">Denman Research Forum web site</a>.</li>
+	<li>You may submit the same abstract for the NMS and Denman Research Forums, however, you will need to register and submit your abstract separately for each of the two events.</li>
+	<li>Group presentations are welcomed. Please upload the abstract under only one group member’s name. The other members of the group should register normally, and, in place of an abstract, upload a note indicating which member of the group submitted the official abstract.</li>
+	<li>Deadline for NMS Undergraduate Research Forum abstract submissions: 5:00 pm, April 4, 2011.</li>
+	<li>All students will be notified as to whether their abstract is accepted for presentation by April 15, 2011.</li>
+</ul>
 
 <? bottom(); ?>
---
+++ research-forum/tips.php
@@ -0,0 +1,18 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('NMS Undergraduate Research Forum', 'Abstract Writing and Poster Design', 'no-navbar');
+menu('research-forum');
+?>
+
+<ul>
+	<li><a target="_blank" href="research-forum/Abstract%20Guidelines.pdf">Abstract preparation guidelines</a></li>
+	<li><a target="_blank" href="http://www.undergraduateresearch.osu.edu/">OSU Undergraduate Research Office</a></li>
+	<li><a target="_blank" href="http://denman.osu.edu/workshops.aspx">OSU poster preparation workshops</a></li>
+	<li><a target="_blank" href="http://undergraduateresearch.osu.edu/nutsandbolts/posters/index.htm">Organizing your poster</a></li>
+	<li><a target="_blank" href="http://undergraduateresearch.osu.edu/nutsandbolts/posters/posters4.htm">Printing your poster</a></li>
+	<li><a target="_blank" href="http://www.swarthmore.edu/NatSci/cpurrin1/posteradvice.htm">Additional poster preparation advice</a></li>
+	<li><a target="_blank" href="http://www.ploscompbiol.org/article/info%3Adoi%2F10.1371%2Fjournal.pcbi.0030102">Ten Simple Rules for a Good Poster Presentation</a></li>
+</ul>
+
+<? bottom(); ?>
