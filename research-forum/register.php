5490d3375b0ff639b48c66cd3be6383b0c51dc73
mikegee <gee.24@osu.edu>
2011-03-08 14:05:12 -0500

    link deeper to avoid confusion

--- research-forum/register.php
+++ research-forum/register.php
@@ -6,7 +6,7 @@ menu('research-forum');
 ?>
 
 <ul>
-	<li>To submit your abstract and register to present a research poster at the April 29, 2011 NMS Undergraduate Research Forum, <a href="https://ruby.asc.ohio-state.edu/bio">click here</a>.</li>
+	<li>To submit your abstract and register to present a research poster at the April 29, 2011 NMS Undergraduate Research Forum, <a href="https://ruby.asc.ohio-state.edu/bio/forum_application/new">click here</a>.</li>
 	<li>Information about how to write an abstract is available at the <a href="http://denman.osu.edu/guidlines.aspx">Denman Research Forum web site</a>.</li>
 	<li>You may submit the same abstract for the NMS and Denman Research Forums, however, you will need to register and submit your abstract separately for each of the two events.</li>
 	<li>Group presentations are welcomed. Please upload the abstract under only one group member’s name. The other members of the group should register normally, and, in place of an abstract, upload a note indicating which member of the group submitted the official abstract.</li>
