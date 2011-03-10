ace781afaf97855143200ad0e098e56dfa22529b
mikegee <gee.24@osu.edu>
2011-03-10 11:00:17 -0500

    verbiage updates as per Caroline

--- courses/index.php
+++ courses/index.php
@@ -5,20 +5,26 @@ top('Courses');
 menu('courses');
 ?>
 <h1>Mission Statement</h1>
-<p>The Mission of the Center for Life Sciences Education (CLSE) is to develop, teach, and provide
-   administrative support for introductory level Biology courses taught by the College of Biological
-   Sciences. These courses include those that satisfy the General Education Curriculum (GEC)
-   requirement for students who are not majoring in the biological sciences (e.g., Biology 101, 102),
-   courses for students who are planning on majoring in one of the biological sciences (Biology 113,
-   114), an Honors sequence (Biology H115, H116), and any other Biology courses that might be
-   developed and taught. The introductory level majors and non-majors courses taught by the CLSE are
-   an important component of the GEC of the Colleges of the Arts and Sciences. Within the paradigm of
-   an arts and sciences curriculum, it is the mission of the CLSE to provide courses that:</p>
+<p>The Mission of the Center for Life Sciences Education (CLSE) is to
+	 develop, teach, and provide administrative support for the biology major
+   and introductory level Biology courses at the Ohio State University. CLSE
+   courses include those that satisfy the General Education Curriculum (GEC)
+	 requirement for students who are not majoring in the biological sciences
+	 (e.g., Biology 101, 102), introductory courses for students who are
+	 planning on majoring in one of the sciences (Biology 113, 114), an Honors
+	 sequence (Biology H115, H116), some of the courses that satisfy biology
+	 major requirements (Biology 401, 402), and any other Biology courses that
+	 might be developed and taught. The introductory level majors and
+	 non-majors courses taught by the CLSE are an important component of the
+	 GEC of the College of the Arts and Sciences. Within the paradigm of an
+	 arts and sciences curriculum, it is the mission of the CLSE to provide
+   courses that:</p>
 <ul>
  <li>meet the interests and educational needs of the diverse population of students served,</li>
  <li>are academically challenging and rewarding,</li>
  <li>are of the highest quality in both content and presentation.</li>
 </ul>
+
 <h1>Available Courses</h1>
 <ul>
  <li>Biology 101 &ndash; Introductory Biology</li>
@@ -27,5 +33,8 @@ menu('courses');
  <li>Biology 114 &ndash; Form, Function, Diversity, and Ecology</li>
  <li>Honors Biology 115 &ndash; Honors Biology I</li>
  <li>Honors Biology 116 &ndash; Honors Biology II</li>
+ <li>Biology 401 &ndash; Integrated Biology I</li>
+ <li>Biology 402 &ndash; Integrated Biology II</li>
 </ul>
+
 <? bottom(); ?>
--- index.php
+++ index.php
@@ -5,20 +5,19 @@ no_menu();
 ?>
 
 <p>The Center for Life Sciences Education is an interdepartmental program
-   administered by the <a href="http://www.biosci.ohio-state.edu/">College of
-   Biological Sciences</a> at The Ohio State University. The Center's 
+   administered by the <a href="http://artsandsciences.osu.edu/">College of
+   Arts and Sciences</a> at The Ohio State University. The Center's
    mission is to develop, teach, and provide administrative support for
-   the Biology major and the Biology courses taught by the College of Biological
-   Sciences.</p>
-<p>The introductory level majors and non-majors courses taught by the CLSE are
-   an important component of the GEC of the Colleges of the Arts and Sciences.
-   Within the paradigm of an arts and sciences curriculum, it is the mission of
-   the CLSE to provide courses that:</p>
+   the Biology major and Biology courses.</p>
+
+<p>The introductory level majors and non-majors courses taught by the CLSE
+	 are an important component of the GEC of the College of the Arts and
+	 Sciences. Within the paradigm of an arts and sciences curriculum, it is
+   the mission of the CLSE to provide courses that:</p>
 <ul>
  <li>meet the interests and needs of the diverse population of students served,</li>
  <li>are academically challenging and rewarding,</li>
  <li>are of the highest quality in both content and presentation.</li>
 </ul>
-
 
 <? bottom(); ?>
