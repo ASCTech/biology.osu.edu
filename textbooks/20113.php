143b7426a5f0d10f85a5fc3d4225345afb1cf525
mikegee <gee.24@osu.edu>
2011-06-20 13:36:28 -0400

    SU11 Textbooks

---
+++ textbooks/20113.php
@@ -0,0 +1,145 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Textbooks','Summer 2011');
+menu('textbooks');
+?>
+<table class="textbooks">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Cogan</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-3963-3</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>101N</td>
+  <td>Smock</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-3963-3</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>102</td>
+  <td>Sovic</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>102N</td>
+  <td>Olson</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+	<td>Stiff: The&nbsp;Curious&nbsp;Lives of Human&nbsp;Cadavers
+    <span class="subtext">required</span></td>
+	<td>Roach</td>
+  <td class="isbn">978-0-393324822</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>113</td>
+  <td rowspan="2">Rothacker</td>
+  <td>Biology, 9th&nbsp;edition<span class="subtext">required</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55823-7</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-3972-5</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>114</td>
+  <td>Caesar</td>
+  <td>Biology, 8th&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-8053-6844-4</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>114/116 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-3970-1</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Pigs
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
