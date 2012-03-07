352e4ef8477e63439da8df83a1e43ed66eee178d
mikegee <gee.24@osu.edu>
2012-03-06 21:48:21 -0500

    update deploy script to delete remotely

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -e ssh --archive --omit-dir-times --itemize-changes --no-group --exclude .git --exclude-from .gitignore ./ biology@excelsior:~/public_html/
+rsync -e ssh --archive --delete --delete-excluded --omit-dir-times --itemize-changes --no-group --exclude .git --exclude-from .gitignore ./ biology@excelsior:~/public_html/
