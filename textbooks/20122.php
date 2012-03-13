4a6f4cfc50cacefcb5e39b891879c247eaa611a4
mikegee <gee.24@osu.edu>
2012-03-12 22:08:14 -0400

    SP12 textbooks

---
+++ textbooks/20122.php
@@ -0,0 +1,213 @@
+<?
+chdir('..');
+require_once 'lib/basics.inc';
+top('Textbooks','Spring 2012');
+menu('textbooks');
+?>
+<table class="textbooks">
+ <tr>
+  <th>Course</th>
+  <th>Instructor</th>
+  <th>Textbook</th>
+  <th>Author</th>
+  <th>ISBN</th>
+ </tr>
+ <tr class="gray">
+  <td>101</td>
+  <td>Smock</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4489-7</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>101</td>
+  <td>Cogan</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4489-7</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>101N</td>
+  <td>Gary</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td></td>
+  <td>101 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4489-7</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>102</td>
+  <td>Chordas</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>102N</td>
+  <td>Taylor</td>
+  <td>What&nbsp;is&nbsp;Life? A&nbsp;Guide&nbsp;to&nbsp;Biology w/Physiology, 1st&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Phelan</td>
+  <td class="isbn">978-1-4292-4666-8<br>
+    <span class="subtext">e-book:</span> 1429277955</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>113</td>
+  <td rowspan="2">Swenson &amp; Bernot</td>
+  <td>Biology, 9th&nbsp;edition with Mastering <span class="subtext">required</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55814-5</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4498-9</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>113X</td>
+  <td rowspan="2">Stetson</td>
+  <td>Biology, 9th&nbsp;edition <span class="subtext">required</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55823-7</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4498-9</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>114</td>
+  <td>Taylor</td>
+  <td>Biology, 9th&nbsp;edition <span class="subtext">recommended</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55823-7</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>114/116 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4496-5</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Pigs
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>115</td>
+  <td rowspan="2">Dean</td>
+  <td>Biology, 9th&nbsp;edition <span class="subtext">required</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55823-7</td>
+ </tr>
+ <tr class="gray">
+  <td></td>
+  <td>113/115 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4498-9</td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr>
+  <td>116</td>
+  <td>Wilson</td>
+  <td>Biology, 9th&nbsp;edition <span class="subtext">required</span></td>
+  <td>Campbell</td>
+  <td class="isbn">978-0-321-55823-7</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>114/116 Lab Manual <span class="subtext">required</span></td>
+  <td></td>
+  <td class="isbn">978-0-7380-4496-5</td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Sheep&nbsp;Brain, Pig&nbsp;Heart
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td>Concise&nbsp;Dissection, Pigs
+      <span class="subtext">recommended</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+ <tr><td colspan=5>&nbsp;</td></tr>
+ <tr class="gray">
+  <td>402</td>
+  <td>Herbers &amp; Seeger</td>
+  <td>Turning&nbsp;Techologies ResponseCard&nbsp;RF (clicker)
+      <span class="subtext">required</span></td>
+  <td></td>
+  <td></td>
+ </tr>
+</table>
+<?
+#include 'textbook/bookstores.inc';
+bottom();
+?>
